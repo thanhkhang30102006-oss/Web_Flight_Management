74a2f84f67f3944830a0d23f13096606aefddb10
khangdafin <khangpt.24it@vku.udn.vn>
2025-11-23 07:12:59 +0700

    use sequelize for making model

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+require('dotenv').config();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
