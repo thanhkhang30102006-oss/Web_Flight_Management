e0147f7ecbc7b7c9b89673e35f1235cf14b86aec
khangdafin <khangpt.24it@vku.udn.vn>
2025-12-14 22:00:56 +0700

    Dash board (2 *** 3/3)

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -44,7 +44,7 @@ function DashBoard() {
                 />
                 {/* <StatsComponents /> có thể để ở đây nếu muốn chia cột */}
               </div>
-              <FlightSchedule />
+              <FlightSchedule passengerID={passengerID} />
             </div>
           )}
 
